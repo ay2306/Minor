f29edd66083f086b8675dbfaaccad99621a80f85
ay2306 <mahajan.ayush2306@gmail.com>
2020-11-18 15:22:37 +0530

    abc

--- bot/bot.js
+++ bot/bot.js
@@ -18,38 +18,37 @@ const randomFile = async (fileSize)=>{
     /*
         Generates a random file
     */
-//    const fileName = getRandomString(15);
-//    let fileSize = 1024*1024*20;
-//    console.log('creating file');
-//     await execSync(`python run.py ${fileName} ${fileSize}`);
-//     console.log('created file');
+   const fileName = getRandomString(15);
+   let fileSize = 1024*1024;
+   console.log('creating file');
+    await execSync(`python run.py ${fileName} ${fileSize}`);
+    console.log('created file');
 
-    const browser = await puppeteer.launch({headless:true});   
+    const browser = await puppeteer.launch({headless:false});   
     const page = await browser.newPage();
-    await page.goto('localhost:8050/upload');
-    const element  = await page.$('#fileIn');
-    // element.uploadFile(fileName);
-    element.uploadFile('jlmblsvk4lextmh.txt');
-    // element.uploadFile('Money.Heist.S04E08.720p.English.WebDL.@Amazon_Prime_Video_HD.mp4');
-    const click = await page.$('#uploadClick');
-    console.time('upload');
-    await click.click();
-    await page.waitForNavigation({waitUntil: 'networkidle0'});
-    await page.goto('localhost:8050/upload');
-    console.timeEnd('upload');
     {
+        // await page.goto('localhost:8050/upload');
+        // const element  = await page.$('#fileIn');
+        // element.uploadFile(fileName);
+        // const click = await page.$('#uploadClick');
+        // const start = new Date();
+        // await click.click();
+        // await page.waitForNavigation({waitUntil: 'networkidle0'});
+        // const end = new Date();
+        // console.log("Time Take = ",end-start);
+        // await page.goto('localhost:8050/upload');
     }
-    // {
-    //     await page.goto('localhost:8050/ipfsUpload');
-    //     const element  = await page.$('#fileIn');
-    //     console.time('upload');
-    //     element.uploadFile(fileName);
-    //     // element.uploadFile('Money.Heist.S04E08.720p.English.WebDL.@Amazon_Prime_Video_HD.mp4');
-    //     const click = await page.$('#uploadClick');
-    //     // await click.click();
-    //     await page.waitForNavigation({waitUntil: 'networkidle0'});
-    //     console.timeEnd('upload');
-    // }
+    {
+        await page.goto('localhost:8050/ipfsUpload');
+        const element  = await page.$('#fileIn');
+        console.time('upload');
+        element.uploadFile(fileName);   
+        // element.uploadFile('Money.Heist.S04E08.720p.English.WebDL.@Amazon_Prime_Video_HD.mp4');
+        // const click = await page.$('#uploadClick');
+        // await click.click();
+        await page.waitForNavigation({waitUntil: 'networkidle0'});
+        console.timeEnd('upload');
+    }
 })()
 .then(()=>{
     console.log("Executed Correctly")
--- bot/run.py
+++ bot/run.py
@@ -5,14 +5,6 @@ import random
 
 fileSize = int(sys.argv[2])
 
-arr = []
-
-for i in range(10):
-    arr.append(chr(ord('0')+i))
-for i in range(26):
-    arr.append(chr(ord('a')+i))
-    arr.append(chr(ord('A')+i))
-
 while fileSize > 0:
-    print(arr[random.randint(0,len(arr)-1)],end='')
+    print('A',end='')
     fileSize -= 1
